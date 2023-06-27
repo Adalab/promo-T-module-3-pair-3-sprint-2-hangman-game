c5f8a2cc1d2099086233c20a57744fb22b5804c5
CCarolinaFranco <cirilina@hotmail.com>
2023-06-27 10:30:01 +0100

    props

---
+++ src/components/header/Hearder.js
@@ -0,0 +1,13 @@
+import '../../styles/Header.scss'
+
+
+function Header (){
+
+return(
+   <header>
+    <h1 className='header__title'>Juego del ahorcado</h1>
+  </header>
+)
+}
+
+export default Header;
